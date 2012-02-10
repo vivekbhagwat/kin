39d075ca0e95b314da63f27ce9ada89e819fdbbd
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2012-02-10 00:00:02 -0500

    skeleton for client class

--- lib/kin.rb
+++ lib/kin.rb
@@ -1,15 +1,37 @@
-require 'Sale'
-# require 'Product'
-# require 'SKU'
-# require ''
-
-require 'rubygems'
-require 'net/http'
-require 'uri'
+require 'sale'
 
 
 module Kin
-  def initialize(apikey)
+  class Client
+    attr_accessor :apikey
+    @stores = []
   
+    def initialize(apikey)
+      @apikey = apikey
+    end
+    
+    def active_sales
+      #make request to "https://api.gilt.com/v1/sales/active.json" with apikey
+    end
+    
+    def active_sales_in_store(store_key)
+      #make request to "https://api.gilt.com/v1/sales/" + store_key + "/active.json" with apikey
+    end
+    
+    def upcoming_sales
+      #make request to "https://api.gilt.com/v1/sales/upcoming.json" with apikey
+    end
+    
+    def upcoming_sales_in_store(store_key)
+      #make request to "https://api.gilt.com/v1/sales/" + store_key + "/upcoming.json" with apikey
+    end
+    
+    def sale_detail(store_key, sale_key)
+      #make request to "https://api.gilt.com/v1/sales/" + store_key + "/" + sale_key + "/detail.json" with apikey
+    end
+    
+    def product_detail(product_id)
+      #make request to "https://api.gilt.com/v1/products/" + product_id.toString + "detail.json" with apikey
+    end
   end
 end
