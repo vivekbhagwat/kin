318b79ce869e3d12360b04cbdc784ca4470377c3
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2012-02-10 23:33:47 -0500

    completed tests

--- test/test_kin.rb
+++ test/test_kin.rb
@@ -1,7 +1,59 @@
 require 'helper'
+require 'secret'
 
 class TestKin < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  should "Return a list of active sales" do
+    gilt = Kin::Client.new(Secret.password)
+    actives = gilt.active_sales
+    assert(!actives.empty?)
+    # assert(!(actives.name.empty? || actives.sale.empty? || actives.sale_key.empty?
+          # || actives.store.empty? || actives.sale_url.empty? || actives.begins.empty? || actives.image_urls.empty?))    
   end
+  
+  should "Return a list of active sales for this store" do
+    gilt = Kin::Client.new(Secret.password)
+    Kin.stores.each do |store|
+      active = gilt.active_sales_in_store(store)
+      assert(!active.empty?)
+    end
+  end
+  
+  should "Return a list of upcoming sales" do
+    gilt = Kin::Client.new(Secret.password)
+    upcomings = gilt.upcoming_sales
+    assert(!upcomings.empty?)
+    # assert(!(actives.name.empty? || actives.sale.empty? || actives.sale_key.empty?
+          # || actives.store.empty? || actives.sale_url.empty? || actives.begins.empty? || actives.image_urls.empty?))    
+  end
+  
+  should "Return a list of upcoming sales for this store" do
+    gilt = Kin::Client.new(Secret.password)
+    Kin.stores.each do |store|
+      upcomings = gilt.upcoming_sales_in_store(store)
+      assert(!upcomings.empty?)
+    end
+  end
+  
+  should "Return details about a sale" do
+    gilt = Kin::Client.new(Secret.password)
+    sales = gilt.active_sales
+    list = []
+    sales.each do |sale|
+      list << gilt.sale_detail(sale.store, sale.sale_key)
+    end
+    assert(!list.empty?)
+  end
+  
+  should "Return details about a product" do
+    gilt = Kin::Client.new(Secret.password)
+    sales = gilt.active_sales
+    list = []
+    sales.each do |sale|
+      sale.products.each do |product|
+        list << gilt.product_detail_from_url(product)
+      end
+    end
+    assert(!list.empty?)
+  end
+  
 end
