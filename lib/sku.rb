f4bd97f9c43a17285c17d8e0942af176db95f8f4
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2012-02-10 23:33:58 -0500

    completed sku class

--- lib/sku.rb
+++ lib/sku.rb
@@ -1,2 +1,24 @@
 module Kin
+  class SKU
+    attr_accessor :id, :inventory_status, :msrp_price, :sale_price, :shipping_surcharge, :attributes
+    
+    def initalize(obj={})
+      raise "Invalid Argument" unless obj.is_a?(Hash)
+      initialize(obj['id'], obj['inventory_status'], obj['sale_price'], obj['shipping_surcharge'], obj['attributes'])
+    end
+    
+    def initialize(id=0,inventory_status='',sale_price='',shipping_surcharge='',attributes={})
+      @id = id.to_i
+      @inventory_status = inventory_status.to_s
+      @sale_price = sale_price.to_s
+      @shipping_surcharge = shipping_surcharge.to_s
+      @attributes = attributes
+    end
+    
+    def to_s
+      vars = {}
+      self.instance_variables.map {|v| vars[v.to_sym] = self.instance_variable_get(v)}
+      vars
+    end
+  end
 end
